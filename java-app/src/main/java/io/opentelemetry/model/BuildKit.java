249e5ce43d9135eed3c12014cdaa19800e262fba
Jason Plumb <jplumb@splunk.com>
2025-11-03 17:47:59 -0800

    simplify model

--- java-app/src/main/java/io/opentelemetry/model/BuildKit.java
+++ java-app/src/main/java/io/opentelemetry/model/BuildKit.java
@@ -3,14 +3,12 @@ package io.opentelemetry.model;
 public class BuildKit {
 
   private final Car car;
-  private final String color;
   private final Driver driver;
   //TODO: Mabe add a little fake film narrative for funzies.
 
   public BuildKit(Car car, String color, Driver driver) {
     this.car = car;
     this.driver = driver;
-    this.color = color;
   }
 
   public Car getCar() {
@@ -19,9 +17,5 @@ public class BuildKit {
 
   public Driver getDriver() {
     return driver;
-  }
-
-  public String getColor() {
-    return color;
   }
 }
--- java-app/src/main/java/io/opentelemetry/model/Car.java
+++ java-app/src/main/java/io/opentelemetry/model/Car.java
@@ -5,11 +5,13 @@ public class Car {
   private final int year;
   private final String make;
   private final String model;
+  private final String color;
 
-  public Car(int year, String make, String model) {
+  public Car(int year, String make, String model, String color) {
     this.year = year;
     this.make = make;
     this.model = model;
+    this.color = color;
   }
 
   public int getYear() {
@@ -22,5 +24,9 @@ public class Car {
 
   public String getModel() {
     return model;
+  }
+
+  public String getColor() {
+    return color;
   }
 }
